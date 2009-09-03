f91a6f307f519d2f605a1648f12ce8e2dfae8729
ssmith <ssmith@watson.wustl.edu>
2009-09-03 11:52:05 -0500

    From svn r38668 on 2008-09-17 00:22:52 -0500 (Wed, 17 Sep 2008)
    When modules change @INC, report them.

--- UR/Namespace/Command/Test/Use.pm
+++ UR/Namespace/Command/Test/Use.pm
@@ -47,13 +47,20 @@ sub before {
     my $self = shift;
     $self->{success} = 0;
     $self->{failure} = 0;
+    my %inc = map { $_ => 1 } @INC;
+    $self->{inc} = \%inc;
     $self->SUPER::before(@_);
 }
 
 sub for_each_module_file {
     my $self = shift;
     my $module_file = shift;
+    my %libs_before = map { $_ => 1 } @INC;
     eval "require '$module_file'";
+    my %new_libs = map { $_ => 1 } grep { not $libs_before{$_} } @INC;
+    if (%new_libs) {
+        $self->{rogue_libs}{$module_file} = \%new_libs;
+    }
     if ($@) {
         print "$module_file  FAILED:\n$@\n";
         $self->{failure}++;
@@ -69,6 +76,17 @@ sub after {
     my $self = shift;
     $self->status_message("SUCCESS: $self->{success}");
     $self->status_message("FAILURE: $self->{failure}");
+    
+    my %new = map { $_ => 1 } grep { not $self->{libs}{$_} } @INC;
+    if (%new) {
+        $self->status_message(
+            "ROGUE LIBS: "
+            . Data::Dumper::Dumper($self->{rogue_libs})
+           # . join(", ",sort keys %new)
+           # . "\n"
+        )
+        
+    }
 }
 
 1;
