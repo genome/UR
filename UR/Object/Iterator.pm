3c852b22f0f1710de5345933457d050848ff8921
ssmith <ssmith@watson.wustl.edu>
2009-09-03 11:14:08 -0500

    From svn r35609 on 2008-06-16 19:01:07 -0500 (Mon, 16 Jun 2008)
    Fixed but which used the iterator class instead of the subject class when requesting the iterator underlying callback.

--- UR/Object/Iterator.pm
+++ UR/Object/Iterator.pm
@@ -15,11 +15,11 @@ UR::Object::Type->define(
 sub create_for_filter_rule {
     my $class = shift;
     my $filter_rule = shift;
-    my $code = $UR::Context::current->get_objects_for_class_and_rule($class,$filter_rule,undef,1);
+    my $code = $UR::Context::current->get_objects_for_class_and_rule($filter_rule->subject_class_name,$filter_rule,undef,1);
     
     my $self = $class->SUPER::create(
         # TODO: some bug with frozen items?
-        #filter_rule_id      => $filter_rule->id,        
+        filter_rule_id      => $filter_rule->id,        
     );
     
     $self->_iteration_closure($code);    
