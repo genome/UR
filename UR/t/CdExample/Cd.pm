25b4a07292b4e8f1cd863bc3a0c830adc5ab4877
abrummet <abrummet@watson.wustl.edu>
2009-09-03 14:07:05 -0500

    From svn r48007 on 2009-06-18 11:42:35 -0500 (Thu, 18 Jun 2009)
    Added a new test namespace, CdExample, to actually test out what the UR.pm
    pod purports to be able to do

---
+++ UR/t/CdExample/Cd.pm
@@ -0,0 +1,16 @@
+package CdExample::Cd;
+use CdExample;
+        
+class CdExample::Cd {
+    id_by => 'cd_id',
+    has => [
+        artist => { is => 'CdExample::Artist', id_by => 'artist_id' },
+        title  => { is => 'Text' },
+        year   => { is => 'Integer' },
+        artist_name => { via => 'artist', to => 'name' },
+    ],
+    data_source => 'CdExample::DataSource::DB1',
+    table_name => 'CDS',
+};
+1;
+
