1f60eeec16c7b1481e08bcd78659ebad962a5276
abrummet <abrummet@watson.wustl.edu>
2009-09-03 12:32:51 -0500

    From svn r43043 on 2009-01-27 11:44:53 -0600 (Tue, 27 Jan 2009)
    Fixes for an 'in' operator evaluation against a property that returns an arrayref

--- UR/BoolExpr/Template/PropertyComparison/In.pm
+++ UR/BoolExpr/Template/PropertyComparison/In.pm
@@ -15,10 +15,21 @@ sub evaluate_subject_and_values {
     my $subject = shift;
     my $comparison_value = shift;    
     my $property_name = $self->property_name;    
-    my $property_value = $subject->$property_name;
+    my @property_values = $subject->$property_name;
+
+    if (@property_values == 1 and ref($property_values[0]) == 'ARRAY') {
+        @property_values = @{$property_values[0]};
+    }
+
+    #my $property_value = $subject->$property_name;
     no warnings;
-    for (@$comparison_value) {
-        return 1 if ($property_value eq $_ ? 1 : '');
+    #for (@$comparison_value) {
+    #    return 1 if ($property_value eq $_ ? 1 : '');
+    #}
+    foreach my $comparison_value (@$comparison_value) {
+        foreach my $property_value ( @property_values ) {
+            return 1 if ($property_value eq $comparison_value);
+        }
     }
     return;
 }
