3fede3c18ee13aa03a5609836348fb6471f2edf8
abrummet <abrummet@watson.wustl.edu>
2009-09-03 12:27:46 -0500

    From svn r42784 on 2009-01-16 17:16:48 -0600 (Fri, 16 Jan 2009)
    data_source for the filemux testcase

---
+++ URT/DataSource/SomeFileMux.pm
@@ -0,0 +1,48 @@
+
+package URT::DataSource::SomeFileMux;
+use strict;
+use warnings;
+
+use UR::Object::Type;
+use URT;
+class URT::DataSource::SomeFileMux {
+    is => ['UR::DataSource::FileMux'],
+    type_name => 'urt datasource somefilemux',
+};
+
+sub constant_values { return qw( thing_type ) }
+
+sub required_for_get { return qw( thing_type ) }
+
+sub column_order {
+    return qw( thing_id thing_name thing_color );
+}
+
+sub sort_order {
+    return qw( thing_id );
+}
+
+sub delimiter { "\t" }
+
+BEGIN {
+    our $BASE_PATH = "/tmp/some_filemux_$$/";
+    mkdir $BASE_PATH;
+}
+
+# Note that the file resolver is called as a normal function (with the parameters
+# mentioned in requiret_for_get), not as a method with the data source as the
+# first arg...
+sub file_resolver {
+    my $type = shift;
+    our $BASE_PATH;
+    return "$BASE_PATH/$type";
+}
+
+END { 
+    our $BASE_PATH;
+    my @files = glob("$BASE_PATH/*");
+    unlink @files;
+    rmdir $BASE_PATH;
+}
+
+1;
