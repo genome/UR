7f965db19ae44a646253b99dbbb8cb45c136c727
ssmith <ssmith@watson.wustl.edu>
2009-09-03 12:02:16 -0500

    From svn r39601 on 2008-10-09 13:18:21 -0500 (Thu, 09 Oct 2008)
    cosmetic

--- Command.pm
+++ Command.pm
@@ -25,12 +25,15 @@ UR::Object::Type->define(
         is_output           => { is => 'Boolean', is_optional => 1 },
         is_param            => { is => 'Boolean', is_optional => 1 },
         shell_args_position => { is => 'Integer', is_optional => 1, 
-                                doc => 'when set, this can be used as a positional argument' },
+                                 doc => 'when set, this property is a positional argument when run from a shell' },
     ],
-    has => [
-        bare_args   => { is => 'ARRAY',     is_optional => 1 },
-        is_executed => { is => 'Boolean',   is_optional => 1 },
-        result      => { is => 'Scalar',    is_optional => 1, is_output => 1 },
+    has_optional => [
+        # use the above shell_args_position to map to named args instead
+        bare_args   => { is => 'ARRAY', is_deprecated => 1 },
+        
+        is_executed => { is => 'Boolean' },
+        
+        result      => { is => 'Scalar', is_output => 1 },
     ],
 );
 
@@ -1121,4 +1124,4 @@ for my $type (qw/error warning status debug usage/) {
 1;
 
 #$HeadURL: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk/Command.pm $
-#$Id: Command.pm 39497 2008-10-07 21:17:49Z jwalker $
+#$Id: Command.pm 39601 2008-10-09 18:18:21Z ssmith $
