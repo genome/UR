bd387a18f3dc2e8da0945fe8678ea86a95474c6a
gsanders <gsanders@watson.wustl.edu>
2009-09-03 11:01:21 -0500

    From svn r35121 on 2008-05-29 16:59:50 -0500 (Thu, 29 May 2008)
    Added initialization of bare_args to []. Creation of genome model command
    objects would not work because bare args was only initialized when the command
    line was used.

--- Command.pm
+++ Command.pm
@@ -185,12 +185,13 @@ sub create
 {
     my $class = shift;
     my ($rule,%extra) = $class->get_rule_for_params(@_);
-    if (%extra) {
-        $extra{bare_args} = delete $extra{" "};
-    }
-    my @params_list = $rule->params_list;
+	my @params_list = $rule->params_list;
     my $self = $class->SUPER::create(@params_list, %extra);
-    return unless $self;
+    unless ($self->bare_args) {
+		$self->bare_args([]); 
+	}
+	
+	return unless $self;
 
     # set non-optional boolean flags to false.
     for my $property_meta ($self->_shell_args_property_meta) {
