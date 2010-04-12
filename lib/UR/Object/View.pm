genome/UR

commit 2e47287ce0f0035b7c65fceee49b818a7fe442e3

Spaces interact poorly with the HTML views--they're considered invalid URI characters by XML::LibXSLT--so change them to hyphens.

lib/UR/Object/View.pm
@@ -154,13 +154,13 @@ sub _resolve_view_class_for_params {
             join ("",
                 $vocabulary->convert_to_title_case (
                     map { ucfirst(lc($_)) }
-                    split(/\s+/,$perspective)
+                    split(/-+/,$perspective)
                 )
             ),
             join ("",
                 $vocabulary->convert_to_title_case (
                     map { ucfirst(lc($_)) }
-                    split(/\s+/,$toolkit)
+                    split(/-+/,$toolkit)
                 )
             )
         );

commit dc91ef93fbfadc027d4a24f2f63894606793a6ac

Fix for 'ur describe' failing after commit 2e47287c
Web-based views don't like spaces in the URL, but 'ur describe'
uses spaces in the perspective for its views.  Change the split to
split on either '-'s or whitespace

lib/UR/Object/View.pm
@@ -125,7 +125,7 @@ sub _resolve_view_class_for_params {
     # The subject must be explicitly of class "SubjectClassName" or some subclass of it.
     my $class = shift;
     my %params = $class->define_boolexpr(@_)->params_list;
-   
+
     my $subject_class_name = delete $params{subject_class_name};
     my $perspective = delete $params{perspective};
     my $toolkit = delete $params{toolkit};
@@ -154,13 +154,13 @@ sub _resolve_view_class_for_params {
             join ("",
                 $vocabulary->convert_to_title_case (
                     map { ucfirst(lc($_)) }
-                    split(/-+/,$perspective)
+                    split(/-+|\s+/,$perspective)
                 )
             ),
             join ("",
                 $vocabulary->convert_to_title_case (
                     map { ucfirst(lc($_)) }
-                    split(/-+/,$toolkit)
+                    split(/-+|\s+/,$toolkit)
                 )
             )
         );