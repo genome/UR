373cd64e1ea43ab68133bb7a2abe40d1c091ff33
Nathan Nutter <nnutter@genome.wustl.edu>
2010-07-23 14:35:07 -0500

    updated lib/UR/Namespace/Command/CreateCompletionSpecFile.pm to handle a output file parameter to override default location

--- lib/UR/Namespace/Command/CreateCompletionSpecFile.pm
+++ lib/UR/Namespace/Command/CreateCompletionSpecFile.pm
@@ -16,7 +16,12 @@ UR::Object::Type->define(
         classname => {
             shell_args_position => 1,
             doc => 'The base class to use as trunk of command tree, e.g. Genome::Command or Genome::Model::Tools',
-        }
+        },
+        output => {
+            is => 'Text',
+            is_optional => 1,
+            doc => 'Override output location of the opts spec file.',
+        },
     ]
 );
 
@@ -34,13 +39,21 @@ sub execute {
     (my $module_path) = Getopt::Complete::Cache->module_and_cache_paths_for_package($class, 1);
     my $cache_path = $module_path . ".opts";
 
-    my $fh = IO::File->new('>' . $cache_path) || die "Cannot create file at $cache_path";
+    my $fh;
+    if ($self->output) {
+        $fh = IO::File->new('>' . $self->output) || die "Cannot create file at " . $self->output . "\n";
+    }
+    else {
+        $fh = IO::File->new('>' . $cache_path) || die "Cannot create file at $cache_path\n";
+    }
+    
+    
     if ($fh) {
         my $src = Data::Dumper::Dumper($class->resolve_option_completion_spec());
         $src =~ s/^\$VAR1/\$$class\:\:OPTS_SPEC/;
         $fh->print($src);
     }
-    print "\nOPTS_SPEC file created at $cache_path\n" if $ENV{COMP_LINE};
+    print "\nOPTS_SPEC file created at $cache_path\n";
 }
 
 1;
