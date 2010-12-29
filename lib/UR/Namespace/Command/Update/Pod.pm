bf5ac7cb60e265554914063e74528e21c019e5cb
Thomas Mooney <tmooney@genome.wustl.edu>
2010-12-29 09:37:56 -0600

    A simple command for generating POD for a command and all its subcommands.

---
+++ lib/UR/Namespace/Command/Update/Pod.pm
@@ -0,0 +1,99 @@
+package UR::Namespace::Command::Update::Pod;
+
+use strict;
+use warnings;
+
+use UR;
+use IO::File;
+
+class UR::Namespace::Command::Update::Pod {
+    is => ['UR::Namespace::Command'],
+    has => [
+        base_commands => {
+            is => 'Text',
+            is_many => 1,
+            shell_args_position => 1,
+            doc => 'Generate documentation for these command modules and any subcommand modules'
+        },
+        output_path => {
+            is => 'Text',
+            doc => 'Location to output .pod files',
+        },
+    ],
+    doc => "Generate POD documentation for commands."
+};
+
+sub help_brief {
+    __PACKAGE__->__meta__->doc
+}
+
+sub help_synopsis{
+    return <<"EOS"
+ur update pod UR::Namespace::Command::Update
+ur update pod UR::Namespace::Command::Update::ClassesFromDb UR::Namespace::Command::Update::RenameClass
+EOS
+}
+
+sub help_detail {
+    return join("\n", 
+        'This tool generates POD documentation for each command.',
+        'This command must be run from within the namespace directory.');
+}
+
+sub execute {
+    my $self = shift;
+
+    my @base_commands = $self->base_commands;
+    @base_commands = $self->_class_names_in_tree(@base_commands);
+
+    my @commands = map( $self->get_all_subcommands($_), @base_commands);
+    push @commands, @base_commands;
+
+    for my $command (@commands) {
+        my $pod;
+        eval {
+            $pod = $command->help_usage_command_pod;
+        };
+
+        if($@) {
+            $self->warning_message('Could not generate POD for ' . $command . '. ' . $@);
+            next;
+        }
+
+        unless($pod) {
+            $self->warning_message('No POD generated for ' . $command);
+            next;
+        }
+
+        my $filename = $command->command_name . '.pod';
+        $filename =~ s/ /-/g;
+        my $pod_path = join('/', $self->output_path, $filename);
+
+        my $fh;
+        $fh = IO::File->new('>' . $pod_path) || die "Cannot create file at " . $pod_path . "\n";
+        print $fh $pod;
+        close($fh);
+    }
+
+    return 1;
+}
+
+sub get_all_subcommands {
+    my $self = shift;
+    my $command = shift;
+
+    my @subcommands;
+    eval {
+        @subcommands = $command->sub_command_classes;
+    };
+
+    if($@) {
+        $self->warning_message("Error getting subclasses for module $command: " . $@);
+    }
+
+    return unless @subcommands and $subcommands[0]; #Sometimes sub_command_classes returns 0 instead of the empty list
+
+    return map($self->get_all_subcommands($_), @subcommands), @subcommands;
+}
+
+1;
