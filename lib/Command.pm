c8c3232831eede171353a529838f20c69a25d797
Thomas Mooney <tmooney@genome.wustl.edu>
2010-12-29 09:34:14 -0600

    Include parameter types in POD and HTML versions of help text.

--- lib/Command.pm
+++ lib/Command.pm
@@ -828,10 +828,10 @@ sub help_options
     my $text = '';
     for my $row (@data) {
         if (defined($format) and $format eq 'pod') {
-            $text .= "\n=item " . $row->[0] . "\n\n" . $row->[2] . "\n". ($row->[3]? $row->[3] . "\n" : '');
+            $text .= "\n=item " . $row->[0] . ($row->[1]? '  I<' . $row->[1] . '>' : '') . "\n\n" . $row->[2] . "\n". ($row->[3]? $row->[3] . "\n" : '');
         }
         elsif (defined($format) and $format eq 'html') {
-            $text .= "\n\t<br>" . $row->[0] . "<br> " . $row->[2] . ($row->[3]? "<br>" . $row->[3] : '') . "<br>\n";
+            $text .= "\n\t<br>" . $row->[0] . ($row->[1]? ' <em>' . $row->[1] . '</em>' : '') . "<br> " . $row->[2] . ($row->[3]? "<br>" . $row->[3] : '') . "<br>\n";
         }
         else {
             $text .= sprintf(
