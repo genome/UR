a047444c50840c463a638c06d7a6a19a71779551
Scott Smith <sakoht@cpan.org>
2011-01-15 12:55:59 -0600

    update to version 0.25

--- lib/UR.pm
+++ lib/UR.pm
@@ -8,19 +8,7 @@ use strict;
 use warnings FATAL => 'all';
 
 # Set the version at compile time, since some other modules borrow it.
-use version;
-our $VERSION;
-BEGIN { 
-    # the first CPAN deploy using "version" is failing to install.
-    # this is an attempt to get around it...
-
-    # for the cpan shell, and other parsers
-    $VERSION = 'v0.24';
-
-    # for actual inspection
-    ${VERSION} 
-        = qv('0.24'); 
-};
+our $VERSION = '0.25';
 
 # Ensure we get detailed errors while starting up.
 # This is disabled at the bottom of the module.
