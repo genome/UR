e3b3983bbcf8246ceb331b6f07b1502e6a5faf67
Scott Smith <sakoht@cpan.org>
2011-01-15 09:25:19 -0600

    revered change to generate a temp file inside of the server() method

--- t/URT/DataSource/SomeSQLite.pm
+++ t/URT/DataSource/SomeSQLite.pm
@@ -4,6 +4,16 @@ use strict;
 use warnings;
 
 use File::Temp;
+BEGIN {
+    my $fh = File::Temp->new(TEMPLATE => 'ur_testsuite_db_XXXX',
+                             UNLINK => 0,
+                             SUFFIX => '.sqlite3',
+                             OPEN => 0,
+                             TMPDIR => 1);
+    our $FILE = $fh->filename();
+    $fh->close();
+    # The DB file now exists with 0 size
+}
 
 use UR::Object::Type;
 use URT;
@@ -12,18 +22,16 @@ class URT::DataSource::SomeSQLite {
     type_name => 'urt datasource somesqlite',
 };
 
+END {
+    my @paths_to_remove = map { __PACKAGE__->$_ } qw(server _data_dump_path _schema_path);
+    unlink(@paths_to_remove);
+}
+
 # Standard behavior is to put the DB file right next to the module
 # We'll change that to point to the temp file
 sub server {
-    my $self = shift;
-    our $PATH;
-
-    $PATH ||= File::Temp->new(TEMPLATE => 'ur_testsuite_db_XXXX',
-                              UNLINK => 1,
-                              SUFFIX => $self->_extension_for_db,
-                              OPEN => 0,
-                              TMPDIR => 1);
-    return $PATH->filename;
+    our $FILE;
+    return $FILE;
 }
 
 1;
