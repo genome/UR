116e11fd27b5594b8fc2b6c2bcb650ee169a732f
Brian Derickson <bdericks@genome.wustl.edu>
2011-04-06 12:03:50 -0500

    Revert "added is_sub_command_delegator method to Command::Tree and Command::V2"
    Tony already fixed this and I squashed it. My bad.
    
    This reverts commit a19e3eba1289b15999d806c7c869cc9550390e62.

--- lib/Command/V2.pm
+++ lib/Command/V2.pm
@@ -37,8 +37,6 @@ UR::Object::Type->define(
     ],
 );
 
-sub is_sub_command_delegator { 0 };
-
 sub _is_hidden_in_docs { return; }
 
 sub _init_subclass {
@@ -86,6 +84,10 @@ sub __errors__ {
     return ($self->SUPER::__errors__);
 }
 
+# For compatability with Command::V1 callers
+sub is_sub_command_delegator {
+    return;
+}
 
 sub execute {
     # This is a wrapper for real execute() calls.
