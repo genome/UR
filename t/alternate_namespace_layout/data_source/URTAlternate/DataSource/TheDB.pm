f3b987dbeb2d6923f052ae3c39346fe82ce8efe9
Anthony Brummett <abrummet@genome.wustl.edu>
2011-04-20 16:48:27 -0500

    Make a simple DB with a couple of tables for the test

--- t/alternate_namespace_layout/data_source/URTAlternate/DataSource/TheDB.pm
+++ t/alternate_namespace_layout/data_source/URTAlternate/DataSource/TheDB.pm
@@ -3,34 +3,24 @@ use strict;
 use warnings;
 
 use File::Temp;
-BEGIN {
-    my $fh = File::Temp->new(TEMPLATE => 'ur_testsuite_db_XXXX',
-                             UNLINK => 0,
-                             SUFFIX => '.sqlite3',
-                             OPEN => 0,
-                             TMPDIR => 1);
-    our $FILE = $fh->filename();
-    $fh->close();
-    # The DB file now exists with 0 size
-}
 
-use UR::Object::Type;
 use URTAlternate;
 class URTAlternate::DataSource::TheDB {
     is => ['UR::DataSource::SQLite'],
     type_name => 'urtalterate datasource thedb',
 };
 
-END {
-    my @paths_to_remove = map { __PACKAGE__->$_ } qw(server _data_dump_path _schema_path);
-    unlink(@paths_to_remove);
+sub server {
+    my $self = shift;
+
+    our $PATH;
+    $PATH ||= File::Temp::tmpnam() . '_ur_testsuite_db' . $self->_extension_for_db;
+    return $PATH;
 }
 
-# Standard behavior is to put the DB file right next to the module
-# We'll change that to point to the temp file
-sub server {
-    our $FILE;
-    return $FILE;
+END {
+    our $PATH;
+    unlink $PATH if $PATH;
 }
 
 1;
