07a21216bc2b21c59ae4563e4f3284dffa45eb93
Brian Derickson <bdericks@genome.wustl.edu>
2011-05-17 10:53:33 -0500

    removed debugging code from datasource

--- lib/UR/DataSource.pm
+++ lib/UR/DataSource.pm
@@ -208,7 +208,6 @@ sub _generate_loading_templates_arrayref {
 
     my %obj_joins_by_source_alias;
     if ($obj_joins) {
-        $DB::single = 1;
         my @obj_joins = @$obj_joins;
         while (@obj_joins) {
             my $foreign_alias = shift @obj_joins;
@@ -216,10 +215,6 @@ sub _generate_loading_templates_arrayref {
             for my $foreign_property_name (sort keys %$data) {
                 next if $foreign_property_name eq '-is_required';
                 my $source_alias = $data->{$foreign_property_name}{'link_alias'};
-                if (not defined $source_alias or not defined $foreign_alias) {
-                    $DB::single = 1;
-                    print Data::Dumper::Dumper($foreign_alias, $data);
-                }
                 my $detail = $obj_joins_by_source_alias{$source_alias}{$foreign_alias} ||= {};
                 my $source_property_name = $data->{$foreign_property_name}{'link_property_name'};
                 if ($source_property_name) {
@@ -277,10 +272,6 @@ sub _generate_loading_templates_arrayref {
     for my $template (@templates) {
         next unless $template;  # This join may have resulted in no template?!
         my @id_property_names;
-        unless (defined $template->{data_class_name}) {
-            $DB::single=1;
-            print "No data class name in template: ", Data::Dumper::Dumper($template); 
-        }
         for my $id_class_name ($template->{data_class_name}, $template->{data_class_name}->inheritance) {
             my $id_class_obj = UR::Object::Type->get(class_name => $id_class_name);
             last if @id_property_names = $id_class_obj->id_property_names;
@@ -350,10 +341,6 @@ sub _generate_loading_templates_arrayref {
                 my ($bxt, @values) = $bx->template_and_values();
                 push @$next_joins, [ $bxt->id, \@values, \@value_position_source_property ];
             }
-            print Data::Dumper::Dumper($next_joins);
-            $DB::single = 1;
-        }
-    }        
 
     return \@templates;        
 }
