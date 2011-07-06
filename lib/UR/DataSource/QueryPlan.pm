2a768685feaf43bdef84553cd8875fb523071618
Anthony Brummett <abrummet@genome.wustl.edu>
2011-07-06 14:09:05 -0500

    Better error message when resolving joins and a property's metadata can't be found

--- lib/UR/DataSource/QueryPlan.pm
+++ lib/UR/DataSource/QueryPlan.pm
@@ -1142,6 +1142,9 @@ sub _resolve_object_join_data_for_property_chain {
     # something non-optional
     $is_optional = 0;
     for my $pmeta (@pmeta) {
+        if (!$pmeta) {
+            Carp::croak "Can't resolve joins for ".$rule_template->subject_class_name . " property '$property_name': No property metadata found for that class and property_name";
+        }
         push @joins, $pmeta->_resolve_join_chain();
         $is_optional = 1 if $pmeta->is_optional or $pmeta->is_many;
     }
