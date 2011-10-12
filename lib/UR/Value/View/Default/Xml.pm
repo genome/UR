73388f9142f5d89525bcf14c39c0b09ae4f94d99
Nathan Nutter <iam@nnutter.com>
2011-10-12 12:21:11 -0500

    PerlReference's XML view will now generate XML including the perldata (XML representation of the Perl data)

--- lib/UR/Value/View/Default/Xml.pm
+++ lib/UR/Value/View/Default/Xml.pm
@@ -7,9 +7,29 @@ class UR::Value::View::Default::Xml {
     is => ['UR::Object::View::Default::Xml', 'UR::Value::View::Default::Text'],
 };
 
+sub _generate_xml_doc {
+    my $self = shift;
+
+    my $xml_doc = $self->SUPER::_generate_xml_doc(@_);
+
+    if ($self->subject_class_name->isa('UR::Value::PerlReference')) {
+        $self->_add_perl_data_to_node($self->subject_id);
+    }
+
+    return $xml_doc;
+}
+
 sub _generate_content {
     my $self = shift;
-    my $content = $self->UR::Value::View::Default::Text::_generate_content(@_);
+
+    my $content;
+    if ($self->subject_class_name->isa('UR::Value::PerlReference')) {
+        $content = $self->UR::Object::View::Default::Xml::_generate_content(@_);
+    }
+    else {
+        $content = $self->UR::Value::View::Default::Text::_generate_content(@_);
+    }
+
     return $content; 
 }
 
