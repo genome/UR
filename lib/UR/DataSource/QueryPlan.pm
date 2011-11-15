e5c1b7dc6dd5d29b6a8ac8540a2b2c922814b6a0
Anthony Brummett <abrummet@genome.wustl.edu>
2011-11-15 17:04:27 -0600

    Move the _load() method from UR::Value into UR::DataSource::QueryPlan
    First step of changing UR::Value to use the Default datasource for loading.
    
    QueryPlan isa UR::Value, but we need to retain the ability to miracle
    QueryPlans into existence with _load() early in the query process, rather
    than allowing QueryPlans to get loaded through the regualr loading process.
    The problem is that in order to use the regular loader, you have to generate
    a QueryPlan, which is an issue if you're trying to load a QueryPlan...

--- lib/UR/DataSource/QueryPlan.pm
+++ lib/UR/DataSource/QueryPlan.pm
@@ -84,6 +84,52 @@ class UR::DataSource::QueryPlan {
     ]
 };
 
+
+sub _load {
+        my $class = shift;
+    my $rule = shift;
+
+    # See if the requested object is loaded.
+    my @loaded = $UR::Context::current->get_objects_for_class_and_rule($class,$rule,0);
+    return $class->context_return(@loaded) if @loaded;
+
+    # Auto generate the object on the fly.
+    my $id = $rule->value_for_id;
+    unless (defined $id) {
+        #$DB::single = 1;
+        Carp::croak "No id specified for loading members of an infinite set ($class)!"
+    }
+    my $class_meta = $class->__meta__;
+    my @p = (id => $id);
+    if (my $alt_ids = $class_meta->{id_by}) {
+        if (@$alt_ids == 1) {
+            push @p, $alt_ids->[0] => $id;
+        }
+        else {
+            my ($rule, %extra) = UR::BoolExpr->resolve_normalized($class, $rule);
+            push @p, $rule->params_list;
+        }
+    }
+
+    my $obj = $UR::Context::current->_construct_object($class, @p);
+
+    if (my $method_name = $class_meta->sub_classification_method_name) {
+        my($rule, %extra) = UR::BoolExpr->resolve_normalized($class, $rule);
+        my $sub_class_name = $obj->$method_name;
+        if ($sub_class_name ne $class) {
+            # delegate to the sub-class to create the object
+            $UR::Context::current->_abandon_object($obj);
+            $obj = $UR::Context::current->_construct_object($sub_class_name,$rule);
+            $obj->__signal_change__("load");
+            return $obj;
+        }
+        # fall through if the class names match
+    }
+
+    $obj->__signal_change__("load");
+    return $obj;
+}
+
 # these hash keys are probably removable
 # because they are not above, they will be deleted if _init sets them
 # this exists primarily as a cleanup target list
