3fd00d5101b38cfe5c35cf6084f6bc094381dc3d
Thomas Mooney <tmooney@genome.wustl.edu>
2011-11-17 15:23:52 -0600

    Specify toolkit in HTML views.

--- lib/UR/Object/View/Default/Html.pm
+++ lib/UR/Object/View/Default/Html.pm
@@ -10,7 +10,8 @@ class UR::Object::View::Default::Html {
     is => 'UR::Object::View::Default::Xsl',
     has => {
         output_format => { value => 'html' },
-        transform => { value => 1 }
+        transform => { value => 1 },
+        toolkit => { value => 'html' },
     }
 };
 
