e0b1467aaa0a71be4504d286d2cc2b834ff7fc16
Thomas Mooney <tmooney@genome.wustl.edu>
2011-11-17 15:28:45 -0600

    Add a json perspective for avaialble views.

--- lib/UR/Object/Type/View/AvailableViews/Xml.pm
+++ lib/UR/Object/Type/View/AvailableViews/Xml.pm
@@ -24,6 +24,30 @@ sub _generate_content {
 
     my $target_class = $subject->class_name;
 
+    my %perspectives = $self->_find_perspectives($target_class);
+
+    my $perspectives = $xml_doc->createElement('perspectives');
+    $xml_doc->setDocumentElement($perspectives);
+
+    for my $key (sort keys %perspectives) {
+        my $perspective = $perspectives->addChild( $xml_doc->createElement('perspective') );
+        $perspective->addChild( $xml_doc->createAttribute('name', $key) );
+
+        for my $tool_key (sort keys %{$perspectives{$key}}) {
+            my $toolkit = $perspective->addChild( $xml_doc->createElement('toolkit'));
+            $toolkit->addChild( $xml_doc->createAttribute('name', $tool_key));
+        }
+    }
+
+    $perspectives->addChild( $xml_doc->createAttribute( 'type', $target_class ));
+
+    return $xml_doc->toString(1);
+}
+
+sub _find_perspectives {
+    my $self = shift;
+    my $target_class = shift;
+
     my %perspectives;
     for my $class ($target_class, $target_class->inheritance) {
         next unless $class->isa('UR::Object');
@@ -55,20 +79,7 @@ sub _generate_content {
         }
     }
 
-    my $perspectives = $xml_doc->createElement('perspectives');
-    $xml_doc->setDocumentElement($perspectives);
-
-    for my $key (sort keys %perspectives) {
-        my $perspective = $perspectives->addChild( $xml_doc->createElement('perspective') );
-        $perspective->addChild( $xml_doc->createAttribute('name', $key) );
-
-        for my $tool_key (sort keys %{$perspectives{$key}}) {
-            my $toolkit = $perspective->addChild( $xml_doc->createElement('toolkit'));
-            $toolkit->addChild( $xml_doc->createAttribute('name', $tool_key));
-        }
-    }
-
-    return $xml_doc->toString(1);
+    return %perspectives;
 }
 
 1;
