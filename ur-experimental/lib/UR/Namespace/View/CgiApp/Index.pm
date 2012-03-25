0ceed8cbe4be3ede9f7c84eaf7281a65630ae979
Scott Smith <sakoht@cpan.org>
2012-03-25 12:27:19 -0500

    renamed ur-exp to ur-experimental

---
+++ ur-experimental/lib/UR/Namespace/View/CgiApp/Index.pm
@@ -0,0 +1,21 @@
+package UR::Namespace::View::SchemaBrowser::CgiApp::Index;
+
+use strict;
+use warnings;
+require UR;
+our $VERSION = "0.38"; # UR $VERSION;
+
+use base 'UR::Namespace::View::SchemaBrowser::CgiApp::Base';
+
+
+sub _template{q(
+<HTML>
+<HEAD><TITLE>Class/Schema Browser</TITLE></HEAD>
+<BODY>
+<A HREF="schema.html">Browse the Schema</A><BR>
+<A HREF="class.html">Browse the Classes</A>
+</BODY>
+</HTML>
+)};
+
+1;
