1f2f526373764635d3381d99b485f663d0a16cea
Anthony Brummett <abrummet@genome.wustl.edu>
2012-06-06 14:53:32 -0500

    Work even if the # of linking properties is different
    For object accessor properties, if the number of id_by properties does not
    match the number of id properties (meaning it holds the composite ID value
    that refers to an instance of a class with multiple ID properties), then
    trap the exception and print out something that's still useful

--- lib/UR/Object/Property/View/ReferenceDescription/Text.pm
+++ lib/UR/Object/Property/View/ReferenceDescription/Text.pm
@@ -20,18 +20,28 @@ sub _update_view_from_subject {
     my $r_class_name = $property_meta->data_type;
 
     my @relation_detail;
-    foreach my $pair ( $property_meta->get_property_name_pairs_for_join() ) {
-        my($property_name, $r_property_name) = @$pair;
-        push @relation_detail, "$r_property_name => \$self->$property_name";
+    my @pairs = eval { $property_meta->get_property_name_pairs_for_join() };
+
+    my $text;
+    if (@pairs) {
+        foreach my $pair ( @pairs ) {
+            my($property_name, $r_property_name) = @$pair;
+            push @relation_detail, "$r_property_name => \$self->$property_name";
+        }
+        my $padding = length($r_class_name) + 34;
+        my $relation_detail = join(",\n" . " "x$padding, @relation_detail);
+
+        $text = sprintf("  %22s => %s->get(%s)\n",
+                        $property_meta->property_name,
+                        $r_class_name,
+                        $relation_detail);
+    } else {
+        $text = sprintf(" %22s => %s->get(id => \$self->%s)\n",
+                        $property_meta->property_name,
+                        $r_class_name,
+                        $property_meta->property_name);
     }
-    my $padding = length($r_class_name) + 34;
-    my $relation_detail = join(",\n" . " "x$padding, @relation_detail);
-
-
-    my $text = sprintf("  %22s => %s->get(%s)\n",
-                       $property_meta->property_name,
-                       $r_class_name,
-                       $relation_detail);
+            
     my $widget = $self->widget();
     my $buffer_ref = $widget->[0];
     $$buffer_ref = $text;
