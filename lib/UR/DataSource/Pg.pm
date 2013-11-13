7d0c7e33a0e0f2c6089ba237d6a1f649a2f7b730
Anthony Brummett <abrummet@genome.wustl.edu>
2013-11-13 15:58:21 -0600

    Use the Oracle/Pg version of _value_is_null() for all RDBMS data source
    More consistency
    
    Change-Id: Ia8893fe5da469ae4d540a0ceb751f711ecc862c3

--- lib/UR/DataSource/Pg.pm
+++ lib/UR/DataSource/Pg.pm
@@ -168,15 +168,6 @@ sub _alter_sth_for_selecting_blob_columns {
     }
 }
 
-sub _value_is_null {
-    my ($class,$value) = @_;
-    return 1 if not defined $value;
-    return 1 if $value eq '';
-    return 1 if (ref($value) eq 'HASH' and $value->{operator} eq '=' and (!defied($value->{value}) or $value->{value} eq ''));
-    return 0;
-}   
-
-
 my $DATE_FORMAT = 'YYYY-MM-DD HH24:MI:SS';
 my $TIMESTAMP_FORMAT = 'YYYY-MM-DD HH24:MI:SS.US';
 sub cast_for_data_conversion {
