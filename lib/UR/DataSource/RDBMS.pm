cf8f24867897ef8700d91fc5e0c49448dfe9db5e
Anthony Brummett <abrummet@genome.wustl.edu>
2014-03-21 10:55:17 -0500

    Switch to using a method call
    Further changes will require other method calls within
    
    Change-Id: Ief737e13e152fb74ca2fe582820e64524e882edb

--- lib/UR/DataSource/RDBMS.pm
+++ lib/UR/DataSource/RDBMS.pm
@@ -1662,7 +1662,7 @@ sub _create_sub_for_copying_to_alternate_db {
         $ds_type->mk_table_for_class_meta($class_meta, $dbh);
     }
 
-    my @inserter_for_each_table = map { _make_insert_closure_for_loading_template_for_alternate_db($_, $dbh) }
+    my @inserter_for_each_table = map { $self->_make_insert_closure_for_loading_template_for_alternate_db($_, $dbh) }
                                     @saving_templates;
 
     return sub {
@@ -1670,9 +1670,8 @@ sub _create_sub_for_copying_to_alternate_db {
     }
 }
 
-# not a method
 sub _make_insert_closure_for_loading_template_for_alternate_db {
-    my($template, $dbh) = @_;
+    my($self, $template, $dbh) = @_;
 
     my %seen_ids;  # don't insert the same object more than once
 
