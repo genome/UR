4867f672604f0d414b1cf52cd572614a609e10f1
Anthony Brummett <abrummet@genome.wustl.edu>
2015-03-26 15:28:40 -0500

    more documentation for Roles

--- lib/UR/Role.pm
+++ lib/UR/Role.pm
@@ -571,6 +571,7 @@ UR::Role - Roles in UR, an alternative to inheritance
           another_prop  => { is => 'Integer' },
       },
       requires => ['class_method'],
+      excludes => ['Bad::Role'],
   };
   sub role_method { ... }
 
@@ -591,6 +592,126 @@ UR::Role - Roles in UR, an alternative to inheritance
 
 Roles are used to encapsulate a piece of behavior to be used in other classes.
 They have properties and methods that get melded into any class that composes
-them.  A Role can require any composing class to implement a list of methods.
+them.  A Role can require any composing class to implement a list of methods
+or properties.
+
+Roles are not classes.  They can not be instantiated or inherited from.  They
+are composed into a class by listing their names in the C<roles> attribute of
+a class definition.
+
+=head2 Defining a Role
+
+Roles are defined with the C<role> keyword.  Their definition looks very
+similar to a class definition as described in L<UR::Object::Type::Initializer>.
+In particular, Roles have a C<has> section to define properties, and accept
+many class-meta attributes such as 'id_generator', 'valid_signals', and 'doc'.
+
+Roles may implement operator overloading via the 'use overload' mechanism.
+
+Roles also have unique atributes to declare restrictions on their use.
+
+=over 4
+
+=item requires
+
+A listref of property and method names that must appear in any class composing
+the Role.  Properties and methods defined in other roles or parent classes
+can satisfy a requirement.
+
+=item excludes
+
+A listref of Role names that may not be composed together with this Role.
+This is useful to declare incompatibilities between roles.
+
+=back
+
+=head2 Composing a Role
+
+Compose one or more Roles into a class using the 'roles' attribute in a class
+definition.
+
+  class My::Class {
+      roles => ['My::Role', 'Other::Role'],
+      is => ['Parent::Class'],
+      has => ['prop_a','prop_b'],
+  };
+
+Properties and meta-attributes from the Roles get copied into the composing
+class.  Subroutines defined in the Roles' namespaces are imported into the
+class's namespace.  Operator overloads defined in the Roles are applied to
+the class.
+
+=head3 Property and meta-attribute conflicts
+
+An exception is thrown if multiple Roles are composed together that define
+the same property, even if the composing class defines the same property in
+an attempt to override them.
+
+=head3 Method conflicts
+
+An exception is thrown if multiple Roles are composed together that
+define the same subroutine name unless the composing class's namespace also
+has a subroutine with the same name.  The class is expected to implement
+whatever behavior is required, mbaybe by calling one role's method or the
+other, both methods, neither, or anything else.
+
+To call a function in a role, the function's fully qualified name, including
+the role's package, must be used.
+
+=head3 Overload conflicts
+
+Like with method conflicts, an exception is thrown if multiple Roles are
+composed together that overload the same operator unless the composing
+class also overloads that same operator.
+
+An exception is also thrown if composed roles define incompatible 'fallback'
+behavior.  If a role does not specify 'fallback', or explicity sets it to
+C<undef>, it is compatible with other values.  A Role that sets its 'fallback'
+value to true or false is only compatible with other roles' values of undef
+or the same true or false value.
+
+=head2 __import__
+
+Each time a Role is composed into a class, its C<__import__()> method is
+called.  C<__import__()> is passed two arguments:
+
+=over 4
+
+=item *
+
+The name of the role
+
+=item *
+
+The class metadata object composing the role.
+
+=back
+
+This happens after the class is completely constructed.
+
+=head2 Deferred Values
+
+A Role definition may contain DeferredValue objects to act as placeholders
+for values to be filled in when the role is composed into a class.  These
+values are resolved at composition time by calling the named function on
+the composing class.  For example:
+
+  use UR::Role;
+  role ObjectDisplayer {
+      has => [
+          target_object => { is => defer 'target_type' },
+      ]
+  };
+
+  class ShowCars {
+      roles => ['ObjectDisplayer'],
+  };
+  sub ShowCars::target_type { 'Car' }
+
+When the 'target_object' property is composed into the ShowCars class, the
+system calls the method C<ShowCars-E<gt>target_type()> to obtain the value
+'Car' for the data_type of property 'target_object'.
+
+UR::Role exports the function C<defer> to create these DeferredValue objects.
 
 =cut
