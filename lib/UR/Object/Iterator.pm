a0004b0e625c7d180e331d1e12934f0dadf706a0
Anthony Brummett <abrummet@genome.wustl.edu>
2016-01-14 13:39:51 -0600

    remove old, commented-out code

--- lib/UR/Object/Iterator.pm
+++ lib/UR/Object/Iterator.pm
@@ -10,29 +10,6 @@ our @CARP_NOT = qw( UR::Object );
 # These are no longer UR Objects.  They're regular blessed references that
 # get garbage collected in the regular ways
 
-#use UR;
-#
-#UR::Object::Type->define(
-#    class_name      => __PACKAGE__,
-#    has => [
-#        filter_rule_id          => {},
-#    ],
-#);
-#
-#sub create_for_filter_rule {
-#    my $class = shift;
-#    my $filter_rule = shift;
-#    my $code = $UR::Context::current->get_objects_for_class_and_rule($filter_rule->subject_class_name,$filter_rule,undef,1);
-#    
-#    my $self = $class->SUPER::create(
-#        # TODO: some bug with frozen items?
-#        filter_rule_id      => $filter_rule->id,        
-#    );
-#    
-#    $self->_iteration_closure($code);    
-#    return $self;
-#}
-
 sub create {
     die "Don't call UR::Object::Iterator->create(), use create_for_filter_rule() instead";
 }
